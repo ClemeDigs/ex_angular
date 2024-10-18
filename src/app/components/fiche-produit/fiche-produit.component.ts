3d052b517bcde623d49ee84511833c48b3bd4072
Clementine Digout <clementine.digout@yahoo.fr>
2024-10-18 09:49:20 -0400

    first init

---
+++ src/app/components/fiche-produit/fiche-produit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fiche-produit',
+  standalone: true,
+  imports: [],
+  templateUrl: './fiche-produit.component.html',
+  styleUrl: './fiche-produit.component.css'
+})
+export class FicheProduitComponent {
+
+}
